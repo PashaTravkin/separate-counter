57b94a153f8dd837a436a4ea6bf2106141c16bc9
PashaTravkin <refueller2022@bk.ru>
2022-08-28 17:47:15 +0300

    JSON

--- src/Counter/Counter.tsx
+++ src/Counter/Counter.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import s from './Counter.module.css';
 import {IncreaseReset} from "../IncreaseReset/IncreaseReset";
 
